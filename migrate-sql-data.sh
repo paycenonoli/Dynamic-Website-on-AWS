d5ba6f938177c37da81cf67188dcb9eab759d4f9
paycenonoli <119795130+paycenonoli@users.noreply.github.com>
2024-04-08 20:18:03 -0500

    Add files via upload

---
+++ migrate-sql-data.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+S3_URI=s3://ovia-dynamic-sql/V1__shopwise.sql
+RDS_ENDPOINT=<your_endpoint>
+RDS_DB_NAME=<your_DB_name>
+RDS_DB_USERNAME=<your_username>
+RDS_DB_PASSWORD=<your_password>
+
+# Update all packages
+sudo yum update -y
+
+# Download and extract Flyway
+sudo wget -qO- https://download.red-gate.com/maven/release/com/redgate/flyway/flyway-commandline/10.9.1/flyway-commandline-10.9.1-linux-x64.tar.gz | tar -xvz 
+
+# Create a symbolic link to make Flyway accessible globally
+sudo ln -s $(pwd)/flyway-10.9.1/flyway /usr/local/bin
+
+# Create the SQL directory for migrations
+sudo mkdir sql
+
+# Download the migration SQL script from AWS S3
+sudo aws s3 cp "$S3_URI" sql/
+# Run Flyway migration
+flyway -url=jdbc:mysql://"$RDS_ENDPOINT":3306/"$RDS_DB_NAME" \
+  -user="$RDS_DB_USERNAME" \
+  -password="$RDS_DB_PASSWORD" \
+  -locations=filesystem:sql \
+  migrate
